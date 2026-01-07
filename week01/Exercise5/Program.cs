b76612ef3febc6a8a89c79602f58d5a10e074d43
Ticsilva85 <tiago.liverys@gmail.com>
2026-01-07 19:56:16 -0300

    Week 1 Exercise 5 Done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        // Variables created to store the return of the methods
+        string name = PromptUserName();
+        int numFav = PromptUserNumber();
+        int square = SquareNumber(numFav);
+
+        DisplayResult(name, square);
+    }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        
+    static string PromptUserName() 
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter you favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {square}.");
     }
 }
